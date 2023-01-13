19fdb3740076906f8eb4246b52ec123d3ab0521e
SkyFaklist <45099567+SkyFaklist@users.noreply.github.com>
2023-01-14 00:02:55 +0800

    fix bugs

--- compile.sh
+++ compile.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
-#mkdir -p out
+mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-PATH="/workdir/toolchain/bin/:$PATH"
-#make O=out CC=clang $DEVICE ARCH=arm64
+TC_DIR="/workdir/tc-build"
+PATH="$TC_DIR/install/bin/:$PATH"
+make O=out CC=clang $DEVICE ARCH=arm64
 make mrproper
 make -j$(nproc --all) O=out \ CROSS_COMPILE=aarch64-linux-gnu- \ CROSS_COMPILE_ARM32=arm-linux-gnueabi- \ 
-CC=clang \
-AR=llvm-ar \
-OBJDUMP=llvm-objdump \
-STRIP=llvm-strip \
-NM=llvm-nm \
-OBJCOPY=llvm-objcopy \
-LD=ld.lld \
-2>&1 | tee error.log 
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    LD=ld.lld \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        CC=clang \
+        AR=llvm-ar \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip
+        2>&1 | tee error.log
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date=" $starttime" +%s);
 end_seconds=$(date --date="$endtime" +%s);

8a362f74de5c76e021b131ac092248a60270f543
SkyFaklist <45099567+SkyFaklist@users.noreply.github.com>
2023-01-13 12:14:23 +0800

    fix pc

--- compile.sh
+++ compile.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
+mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-TC_DIR="/workdir/tc-build"
-MPATH="$TC_DIR/install/bin/:$PATH"
-rm -f out/arch/arm64/boot/Image.gz-dtb
-PATH="$MPATH" make -j$(nproc) O=out \
-    NM=llvm-nm \
-    OBJCOPY=llvm-objcopy \
-    LD=ld.lld \
-        CROSS_COMPILE=aarch64-linux-gnu- \
-        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-        CC=clang \
-        AR=llvm-ar \
-        OBJDUMP=llvm-objdump \
-        STRIP=llvm-strip
-        2>&1 | tee error.log
+PATH="/workdir/toolchain/bin/:$PATH"
+make O=out CC=clang $DEVICE ARCH=arm64
+make mrproper
+make -j$(nproc --all) O=out \ CROSS_COMPILE=aarch64-linux-gnu- \ CROSS_COMPILE_ARM32=arm-linux-gnueabi- \ 
+CC=clang \
+AR=llvm-ar \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+LD=ld.lld \
+2>&1 | tee error.log 
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date=" $starttime" +%s);
 end_seconds=$(date --date="$endtime" +%s);

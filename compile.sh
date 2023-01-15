a72d15714177c652d8a2c547ecf421e049f3f752
SkyFaklist <45099567+SkyFaklist@users.noreply.github.com>
2023-01-15 13:45:46 +0800

    FIX grammar

--- compile.sh
+++ compile.sh
@@ -5,17 +5,17 @@ export SUBARCH=arm64
 make O=out cepheus_defconfig
 TC_DIR="/workdir"
 PATH="$TC_DIR/toolchain/bin/:$PATH"
-make -j$(nproc --all) O=out \ CROSS_COMPILE=aarch64-linux-gnu- \ CROSS_COMPILE_ARM32=arm-linux-gnueabi- \ 
+make -j$(nproc --all) O=out \
     NM=llvm-nm \
     OBJCOPY=llvm-objcopy \
     LD=ld.lld \
-        CROSS_COMPILE=aarch64-linux-gnu- \
-        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-        CC=clang \
-        AR=llvm-ar \
-        OBJDUMP=llvm-objdump \
-        STRIP=llvm-strip
-        2>&1 | tee error.log
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    CC=clang \
+    AR=llvm-ar \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    2>&1 | tee error.log
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date=" $starttime" +%s);
 end_seconds=$(date --date="$endtime" +%s);
